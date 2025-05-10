cbfbff7c704b9463cb82b422723ea25b4bfa20df
sharvenp <sharvenprasad101@gmail.com>
2025-05-09 23:38:05 -0400

    Add dark theme

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import ClipboardInput from './components/ClipboardInput.vue'
 import ClipboardGrid from './components/ClipboardGrid.vue'
 import ClipboardTile from './components/ClipboardTile.vue'
@@ -12,8 +12,29 @@ interface ClipboardItem {
 const clipboardItems = ref<ClipboardItem[]>([])
 const showThanksToast = ref(false)
 const showHelp = ref(false)
+const isDarkTheme = ref(false)
 
-for (let i = 0; i < 2; i++) {
+// Load theme preference from localStorage
+onMounted(() => {
+  const savedTheme = localStorage.getItem('theme')
+  if (savedTheme === 'dark') {
+    isDarkTheme.value = true
+    document.documentElement.setAttribute('data-theme', 'dark')
+  }
+})
+
+// Watch for theme changes
+watch(isDarkTheme, (newValue) => {
+  if (newValue) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
+  } else {
+    document.documentElement.removeAttribute('data-theme')
+    localStorage.setItem('theme', 'light')
+  }
+})
+
+for (let i = 0; i < 10; i++) {
   addClipboardItem("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
 }
 
@@ -45,9 +66,15 @@ function showThanks() {
 <template>
   <div id="app">
     <div v-if="showThanksToast" class="copied-toast thanks-toast">Thanks for using ClipBuddy!</div>
+    <div class="theme-toggle">
+      <label class="switch">
+        <input type="checkbox" v-model="isDarkTheme">
+        <span class="theme-slider"></span>
+      </label>
+    </div>
     <div class="help-container">
       <button class="help-btn" @click="showHelp = !showHelp" title="Help">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+        <svg class="help-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
       </button>
       <div v-if="showHelp" class="help-tooltip">
         <h3>How to use ClipBuddy:</h3>
@@ -58,6 +85,7 @@ function showThanks() {
           <li>Click the copy button to copy the content</li>
           <li>Click the delete button to remove an item</li>
           <li>Use "Clear All" to remove all items</li>
+          <li>Use the toggle on the top left to change the theme</li>
         </ul>
       </div>
     </div>
