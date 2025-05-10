1d23a3bef49bfe6b4d6c67855adf828315dc1333
sharvenp <sharvenprasad101@gmail.com>
2025-05-09 22:35:00 -0400

    Make it more responsive and fix some themeing

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,11 @@ interface ClipboardItem {
 
 const clipboardItems = ref<ClipboardItem[]>([])
 const showThanksToast = ref(false)
+const showHelp = ref(false)
+
+for (let i = 0; i < 2; i++) {
+  addClipboardItem("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+}
 
 function addClipboardItem(content: string) {
   clipboardItems.value.unshift({
@@ -40,6 +45,22 @@ function showThanks() {
 <template>
   <div id="app">
     <div v-if="showThanksToast" class="copied-toast thanks-toast">Thanks for using ClipBuddy!</div>
+    <div class="help-container">
+      <button class="help-btn" @click="showHelp = !showHelp" title="Help">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+      </button>
+      <div v-if="showHelp" class="help-tooltip">
+        <h3>How to use ClipBuddy:</h3>
+        <ul>
+          <li>Type text in the input field and click "Add" or press Enter to add a new item</li>
+          <li>Double-click any item to copy its content</li>
+          <li>Hover over an item to see copy and delete buttons</li>
+          <li>Click the copy button to copy the content</li>
+          <li>Click the delete button to remove an item</li>
+          <li>Use "Clear All" to remove all items</li>
+        </ul>
+      </div>
+    </div>
     <h1 class="app-title" @dblclick="showThanks">ClipBuddy</h1>
     <div class="input-row">
       <ClipboardInput class="flex-1" @add="addClipboardItem" />
--- src/components/ClipboardTile.vue
+++ src/components/ClipboardTile.vue
@@ -6,14 +6,14 @@
       @click.stop="emit('delete')"
       title="Delete"
     >
-      🗑️
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
     </button>
     <button
       class="copy-btn"
       @click.stop="triggerCopy"
       title="Copy to clipboard"
     >
-      📋
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h167q11-35 43-57.5t70-22.5q40 0 71.5 22.5T594-840h166q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560h-80v120H280v-120h-80v560Zm280-560q17 0 28.5-11.5T520-800q0-17-11.5-28.5T480-840q-17 0-28.5 11.5T440-800q0 17 11.5 28.5T480-760Z"/></svg>
     </button>
     <div class="tile-content">
       {{ content }}
@@ -35,7 +35,7 @@ function handleTileDblClick(e: MouseEvent) {
 function triggerCopy() {
   emit('copy', props.content)
   copied.value = true
-  setTimeout(() => copied.value = false, 1000)
+  setTimeout(() => copied.value = false, 1500)
 }
 </script>
 
